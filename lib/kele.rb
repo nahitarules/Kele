275e91b164e08a4c56e23c7c4e34bec6d3b71f68
nahome <nahom.teshome@yahoo.com>
2018-04-26 19:14:39 -0700

    retrieving user

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
@@ -7,5 +8,10 @@ class Kele
     response = self.class.post('/sessions', body: { "email": email, "password": password})
     @user_auth_token = response["auth_token"]
     raise "Invalid Username or Password." if @user_auth_token.nil?
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: {"authorization" => @user_auth_token})
+    JSON.parse(response.body)
   end
 end
